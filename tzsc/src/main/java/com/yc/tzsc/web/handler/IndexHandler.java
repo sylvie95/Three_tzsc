68bda50776ae8e79701db5f1d09f361b506b9db4
sylvie95 <1451932421@qq.com>
2017-01-03 20:15:50 +0800

    前台部分功能任务分配  ---林师有

---
+++ tzsc/src/main/java/com/yc/tzsc/web/handler/IndexHandler.java
@@ -0,0 +1,10 @@
+package com.yc.tzsc.web.handler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/tzsc")
+public class IndexHandler {
+	
+}
